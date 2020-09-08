60c8cde9710e219a6ab56ceb6bb8c2c3053fd7e5
Edgar Minault <e.minault@bluewin.ch>
2020-09-08 17:24:45 +0200

    SNLI dataset.

--- project_watson/data.py
+++ project_watson/data.py
@@ -1,9 +1,8 @@
 """The aim of this file is to define data import functions"""
 import pandas as pd
 from google.cloud import storage
-
+from nlp import load_dataset
 from project_watson.utils import simple_time_tracker
-
 from project_watson.params import (
     BUCKET_NAME,
     BUCKET_TRAIN_DATA_PATH,
@@ -11,7 +10,6 @@ from project_watson.params import (
     MODEL_VERSION,
     MLFLOW_URI,
 )
-
 
 @simple_time_tracker
 def get_data(nrows=10000, local=False, optimize=False, **kwargs):
@@ -24,10 +22,19 @@ def get_data(nrows=10000, local=False, optimize=False, **kwargs):
     df = pd.read_csv(path, nrows=nrows)
     return df
 
+@simple_time_tracker
+def get_snli(nrows=10000):
+    df = load_dataset("snli")
+    df1 = pd.DataFrame(df['train'])
+    df2 = pd.DataFrame(df['test'])
+    new_df = df1.append(df2, ignore_index=True)
+    new_df['lang_abv'] = 'eng'
+    new_df['language'] = 'English'
+    return new_df.head(nrows)
+
 
 if __name__ == "__main__":
     params = dict(
-        nrows=1000,
-        local=False,  # set to False to get data from GCP (Storage or BigQuery)
+        nrows=1000,  # set to False to get data from GCP (Storage or BigQuery)
     )
-    df = get_data(**params)
+    df = get_snli(**params)
