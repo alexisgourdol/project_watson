6e977141aef350fa192ef0c9074713f6de08bd0e
Edgar Minault <e.minault@bluewin.ch>
2020-09-08 16:35:44 +0200

    Exo hey?

---
+++ project-watson/utils.py
@@ -0,0 +1,19 @@
+import time
+
+################
+#  DECORATORS  #
+################
+
+def simple_time_tracker(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts))
+        else:
+            print(method.__name__, round(te - ts, 2))
+        return result
+
+    return timed
