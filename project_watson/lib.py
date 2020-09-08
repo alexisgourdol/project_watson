0d7eed5b4525ea5c47db68849db4fb7570baaf50
Alexis Gourdol <alexis.gourdol@gmail.com>
2020-09-08 18:35:24 +0200

    passed black to format

--- project_watson/lib.py
+++ project_watson/lib.py
@@ -7,25 +7,24 @@ from os.path import split
 import pandas as pd
 import datetime
 
-pd.set_option('display.width', 200)
+pd.set_option("display.width", 200)
 
 
 def clean_data(data):
-    """ clean data
-    """
+    """clean data"""
     # Remove columns starts with vote
-    cols = [x for x in data.columns if x.find('vote') >= 0]
+    cols = [x for x in data.columns if x.find("vote") >= 0]
     data.drop(cols, axis=1, inplace=True)
     # Remove special characteres from columns
-    data.loc[:, 'civility'] = data['civility'].replace('\.', '', regex=True)
+    data.loc[:, "civility"] = data["civility"].replace("\.", "", regex=True)
     # Calculate Age from day of birth
     actual_year = datetime.datetime.now().year
-    data.loc[:, 'Year_Month'] = pd.to_datetime(data.birthdate)
-    data.loc[:, 'Age'] = actual_year - data['Year_Month'].dt.year
+    data.loc[:, "Year_Month"] = pd.to_datetime(data.birthdate)
+    data.loc[:, "Age"] = actual_year - data["Year_Month"].dt.year
     # Uppercase variable to avoid duplicates
-    data.loc[:, 'city'] = data['city'].str.upper()
+    data.loc[:, "city"] = data["city"].str.upper()
     # Take 2 first digits, 2700 -> 02700 so first two are region
-    data.loc[:, 'postal_code'] = data.postal_code.str.zfill(5).str[0:2]
+    data.loc[:, "postal_code"] = data.postal_code.str.zfill(5).str[0:2]
     # Remove columns with more than 50% of nans
     cnans = data.shape[0] / 2
     data = data.dropna(thresh=cnans, axis=1)
@@ -33,25 +32,34 @@ def clean_data(data):
     rnans = data.shape[1] / 2
     data = data.dropna(thresh=rnans, axis=0)
     # Discretize based on quantiles
-    data.loc[:, 'duration'] = pd.qcut(data['surveyduration'], 10)
+    data.loc[:, "duration"] = pd.qcut(data["surveyduration"], 10)
     # Discretize based on values
-    data.loc[:, 'Age'] = pd.cut(data['Age'], 10)
+    data.loc[:, "Age"] = pd.cut(data["Age"], 10)
     # Rename columns
-    data.rename(columns={'q1': 'Frequency'}, inplace=True)
+    data.rename(columns={"q1": "Frequency"}, inplace=True)
     # Transform type of columns
-    data.loc[:, 'Frequency'] = data['Frequency'].astype(int)
+    data.loc[:, "Frequency"] = data["Frequency"].astype(int)
     # Rename values in rows
-    drows = {1: 'Manytimes', 2: 'Onetimebyday', 3: '5/6timesforweek',
-             4: '4timesforweek', 5: '1/3timesforweek', 6: '1timeformonth',
-             7: '1/trimestre', 8: 'Less', 9: 'Never'}
-    data.loc[:, 'Frequency'] = data['Frequency'].map(drows)
+    drows = {
+        1: "Manytimes",
+        2: "Onetimebyday",
+        3: "5/6timesforweek",
+        4: "4timesforweek",
+        5: "1/3timesforweek",
+        6: "1timeformonth",
+        7: "1/trimestre",
+        8: "Less",
+        9: "Never",
+    }
+    data.loc[:, "Frequency"] = data["Frequency"].map(drows)
     return data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # For introspections purpose to quickly get this functions on ipython
     import project_watson
+
     folder_source, _ = split(project_watson.__file__)
-    df = pd.read_csv('{}/data/data.csv.gz'.format(folder_source))
+    df = pd.read_csv("{}/data/data.csv.gz".format(folder_source))
     clean_data = clean_data(df)
-    print(' dataframe cleaned')
+    print(" dataframe cleaned")
--- project_watson/translation.py
+++ project_watson/translation.py
@@ -7,24 +7,26 @@ import pandas as pd
 from googletrans import Translator
 from dask import bag, diagnostics
 
+
 def translate(words, dest):
     """Translates words to a language that is NOT english.
     If none is provided, a random language is chosen"""
-    dest_choices = ['zh-cn',
-                    'ar',
-                    'fr',
-                    'sw',
-                    'ur',
-                    'vi',
-                    'ru',
-                    'hi',
-                    'el',
-                    'th',
-                    'es',
-                    'de',
-                    'tr',
-                    'bg'
-                    ]
+    dest_choices = [
+        "zh-cn",
+        "ar",
+        "fr",
+        "sw",
+        "ur",
+        "vi",
+        "ru",
+        "hi",
+        "el",
+        "th",
+        "es",
+        "de",
+        "tr",
+        "bg",
+    ]
     if not dest:
         dest = np.random.choice(dest_choices)
 
@@ -32,24 +34,34 @@ def translate(words, dest):
     decoded = translator.translate(words, dest=dest).text
     return decoded
 
+
 def trans_parallel(df, dest):
     premise_bag = bag.from_sequence(df.premise.tolist()).map(translate, dest)
-    hypo_bag =  bag.from_sequence(df.hypothesis.tolist()).map(translate, dest)
+    hypo_bag = bag.from_sequence(df.hypothesis.tolist()).map(translate, dest)
     with diagnostics.ProgressBar():
         premises = premise_bag.compute()
         hypos = hypo_bag.compute()
-    df[['premise', 'hypothesis']] = list(zip(premises, hypos))
+    df[["premise", "hypothesis"]] = list(zip(premises, hypos))
     return df
 
-if __name__ == '__main__':
-    words_test_en = 'This is a very English sentence'
-    words_test_fr = 'Cette phrase est une belle phrase en français'
 
-    print('Test translate EN, dest EN ==========')
-    print(translate(words_test_en, 'en'))
-    print(' ')
-    print('Test translate FR, dest EN ==========')
-    print(translate(words_test_fr, 'en'))
-    print(' ')
+if __name__ == "__main__":
+    words_test_en = "This is a very English sentence"
+    words_test_fr = "Cette phrase est une belle phrase en français"
+
+    print("Test translate EN, dest EN ==========")
+    print(translate(words_test_en, "en"))
+    print(" ")
+    print("Test translate FR, dest EN ==========")
+    print(translate(words_test_fr, "en"))
+    print(" ")
+
+    df = pd.read_csv('data/train.csv')
+    df = df.head(20)
+    print(df)
+
+
+
+
 
 
