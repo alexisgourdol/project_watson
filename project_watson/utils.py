ab31b00a3b875eddd9c3f3cd2f170ad90e13c2bc
Edgar Minault <e.minault@bluewin.ch>
2020-09-11 14:39:08 +0200

    Initialised app and modified utils.

--- project_watson/utils.py
+++ project_watson/utils.py
@@ -6,6 +6,7 @@ import time
 
 
 def simple_time_tracker(method):
+    """Time tracker to check the fitting times when training the models."""
     def timed(*args, **kw):
         ts = time.time()
         result = method(*args, **kw)
@@ -18,3 +19,15 @@ def simple_time_tracker(method):
         return result
 
     return timed
+
+def geocoder_here(adress, token=HERE_API_KEY):
+    """
+    adress: 4 Av du General de Gaulle
+     ==>  {'Latitude': 48.85395, 'Longitude': 2.27758}
+    """
+    geocoderApi = herepy.GeocoderApi(api_key=token)
+    res = geocoderApi.free_form(adress)
+    res = res.as_dict()
+    coords = res["Response"]["View"][0]["Result"][0]["Location"]["DisplayPosition"]
+    coords = {k.lower(): v for k, v in coords.items()}
+    return coords
