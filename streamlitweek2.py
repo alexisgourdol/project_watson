42f6603521933e85da67fbba1e41139f69802127
Leo Baudinet <leo_baudinet@hotmail.fr>
2020-09-14 12:12:55 +0200

    ok

---
+++ streamlitweek2.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+#IMPORT DATA
+data = pd.read_csv("project_watson/data/train.csv")
+
+#TITLE
+st.write("""
+# Project Watson
+Hello *world!*
+""")
+
+#SLIDEBAR
+st.sidebar.header("Pick a language")
+
+def user_input_language():
+    idiom = st.sidebar.selectbox(
+        "",
+        ([
+            "Arabic",
+            "Bulgarian",
+            "Chinese",
+            "English",
+            "French",
+            "German",
+            "Greek",
+            "Hindi",
+            "Russian",
+            "Spanish",
+            "Swahili",
+            "Thai",
+            "Turkish",
+            "Urdu",
+            "Vietnamese"
+            ])
+        )
+    return idiom
+
+idiom = user_input_language()
+
+#INPUT PARAMETERS
+st.subheader('User Input parameters')
+
+def two_sentences(idiom):
+    premise = data.premise[data.language == idiom].reset_index(drop=True)[np.random.randint(0,len(idiom))]
+    hypothesis = data[data['premise'] == premise].hypothesis.values[0]
+    return premise, hypothesis
+
+df_two_sentences = pd.DataFrame(two_sentences(idiom), index=["premise", "hypothesis"]).T
+
+st.table(df_two_sentences.assign(hack="").set_index("hack"))
+
+#CLASS LABELS
+st.subheader('Class labels')
+
+st.write(pd.DataFrame(data={
+    "label" : [0,1,2],
+    "evaluation" : ["Entailment","Neutral","Contradiction"]
+    }).set_index("evaluation"))
