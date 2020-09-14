04f925c4359baaa2fbfb749056d1f42e17114144
Leo Baudinet <leo_baudinet@hotmail.fr>
2020-09-14 12:32:44 +0200

    ok refacto

--- streamlitweek2.py
+++ streamlitweek2.py
@@ -2,18 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-#IMPORT DATA
-data = pd.read_csv("project_watson/data/train.csv")
-
-#TITLE
-st.write("""
-# Project Watson
-Hello *world!*
-""")
-
-#SLIDEBAR
-st.sidebar.header("Pick a language")
-
 def user_input_language():
     idiom = st.sidebar.selectbox(
         "",
@@ -37,27 +25,34 @@ def user_input_language():
         )
     return idiom
 
-idiom = user_input_language()
-
-#INPUT PARAMETERS
-st.subheader('User Input parameters')
-
 def two_sentences(idiom):
+    data = pd.read_csv("project_watson/data/train.csv")
     premise = data.premise[data.language == idiom].reset_index(drop=True)[np.random.randint(0,len(idiom))]
     hypothesis = data[data['premise'] == premise].hypothesis.values[0]
     return premise, hypothesis
 
-df_two_sentences = pd.DataFrame(two_sentences(idiom), index=["premise", "hypothesis"]).T
 
-st.table(df_two_sentences.assign(hack="").set_index("hack"))
+def main():
+    #TITLE
+    st.write("""
+    # Project Watson
+    Hello *world!*
+    """)
+    #SLIDEBAR
+    st.sidebar.header("Pick a language")
+    idiom = user_input_language()
 
-#CLASS LABELS
-st.subheader('Class labels')
+    #INPUT PARAMETERS
+    st.subheader('User Input parameters')
+    df_two_sentences = pd.DataFrame(two_sentences(idiom), index=["premise", "hypothesis"]).T
+    st.table(df_two_sentences.assign(hack="").set_index("hack"))
 
-st.write(pd.DataFrame(data={
-    "label" : [0,1,2],
-    "evaluation" : ["Entailment","Neutral","Contradiction"]
-    }).set_index("evaluation"))
+    #CLASS LABELS
+    st.subheader('Class labels')
+    st.write(pd.DataFrame(data={
+        "label" : [0,1,2],
+        "evaluation" : ["Entailment","Neutral","Contradiction"]
+        }).set_index("evaluation"))
 
 if __name__ == "__main__":
     main()
